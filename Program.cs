64ba49786d749bfb73e9db40f0c5a129c2c509be
RaccoonJ <j0542351@gmail.com>
2023-01-16 13:53:47 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance");
+            Console.WriteLine("-----------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to exit . . .");
+            Console.ReadKey(true);
+        }
+    }
+}
